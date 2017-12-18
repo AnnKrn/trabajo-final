95d81746e1848568418b771e3ffa7a70ce1f39d1
joyloove <jocelyn.esquivel24@gmail.com>
2017-12-17 22:08:13 -0600

    las 4 gráficas

--- js/app.js
+++ js/app.js
@@ -65,3 +65,112 @@ var myDoughnutChart = new Chart(ctx, {
     data: data,
     options: options
 });
+
+var ctx = document.getElementById("myBars");
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '% técnico',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+var ctx = document.getElementById('myLines').getContext("2d");
+
+var barStroke = ctx.createLinearGradient(700, 0, 120, 0);
+barStroke.addColorStop(0, 'rgba(0, 255, 188, 0.6)');
+barStroke.addColorStop(1, 'rgba(0, 205, 194, 0.6)');
+
+var barFill = ctx.createLinearGradient(700, 0, 120, 0);
+barFill.addColorStop(0, "rgba(0, 255, 188, 0.6)");
+barFill.addColorStop(1, "rgba(0, 205, 194, 0.6)");
+
+var barFillHover = ctx.createLinearGradient(700, 0, 120, 0);
+barFillHover.addColorStop(0, "rgba(0, 255, 188, 0.8)");
+barFillHover.addColorStop(1, "rgba(0, 205, 194, 0.6)");
+
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ["Sprint 1", "Sprint 2", "Sprint 3", "Sprint 4", "Sprint 5"],
+        datasets: [{
+            label: "Sprint",
+            borderColor: barStroke,
+			borderWidth: 1,
+            fill: true,
+            backgroundColor: barFill,
+			hoverBackgroundColor: barFillHover,
+            data: [100, 50, 60, 80, 70]
+        }]
+    },
+    options: {
+        animation: {
+            easing: "easeOutQuart"
+        },
+        legend: {
+            position: "bottom",
+			display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "##555555",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    padding: 15,
+					//display: false - remove this and commenting to display: false
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false,
+					color: "#555555",
+					zeroLineColor: "#555555"
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+					display: false,
+					color: "#555555",
+					zeroLineColor: "#555555"
+                },
+                ticks: {
+                    padding: 15,
+					beginAtZero: true,
+                    fontColor: "#555555",
+                    fontStyle: "bold",
+					maxTicksLimit: 20,
+					//display: false - remove this and commenting to display: false
+                }
+            }]
+        }
+    }
+});
